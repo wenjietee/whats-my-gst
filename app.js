0316eec36ac2e62fa0a67ad0e5e748fbe5880344
Wenjie <wenjietj@gmail.com>
2021-01-24 01:31:13 +0800

    created boilerplate and starter code

--- app.js
+++ app.js
@@ -1,66 +1,25 @@
-// object to store calculator values
-const calculatorValues = {
-	display: '',
-	currentValue: 0,
+// object to store app values
+const appValues = {
+	// user values
+	myAmount: 0,
+	myTotal: 0,
+	myGstAmount: 0,
+	myServiceChargeAmount: 0,
+	// gst and service charge rates
+	gstRate: undefined,
+	serviceCharge: 10,
 };
 
-// calulate the amount between 2 values via chosen operator
-// const calculate = (value1, value2, operator) => {
-// 	initial method
-// 	switch (operator) {
-// 		case '+':
-// 			return value1 + value2;
-// 			break;
-// 		case '-':
-// 			return value1 - value2;
-// 			break;
-// 		case '*':
-// 			return value1 * value2;
-// 			break;
-// 		case '/':
-// 			return value1 / value2;
-// 			break;
-// 		default:
-// 			console.log('Invalid operation');
-// 	}
-// };
+// get gst from data.gov.sg
+const ajaxGetGST = () => {};
 
-// attempting new calculate method based on eval() method
-const calculate = (input) => {
-	// try catch to catch errors thrown by eval() method
-	try {
-		calculatorValues.currentValue = eval(input);
-		return calculatorValues.currentValue;
-	} catch (err) {
-		calculatorValues.currentValue = 'Invalid Operation!';
-		return calculatorValues.currentValue;
-	}
-};
+// calculate gst and service charge or both
+const calculateMyServiceCharge = () => {};
 
-// clear stored value from calculatorValues
-const clear = () => {
-	calculatorValues.display = '';
-	calculatorValues.currentValue = 0;
-	$('#value').empty();
-};
+const calculateMyGST = () => {};
+
+// remove last value (backspace)
+const removeLastValue = () => {};
 
 // start prog
-$(() => {
-	//init calculatorValues on display div
-	$('#value').text(calculatorValues.display);
-
-	//keypad click event
-	$('.keypad').on('click', (event) => {
-		$('#value').empty();
-		calculatorValues.display += $(event.currentTarget).text();
-		$('#value').text(calculatorValues.display);
-	});
-
-	// clear button event
-	$('#clear').on('click', clear);
-
-	// equal button event
-	$('#equal').on('click', () => {
-		$('#value').text(calculate(calculatorValues.display));
-	});
-});
+$(() => {});
