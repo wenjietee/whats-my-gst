a49258d2eaea1ebc4a5af99af420302ad3a4acc5
Wenjie <wenjietj@gmail.com>
2021-01-24 13:48:27 +0800

    added ajax call to retrieve gst rate

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-// object to store app values
-const appValues = {
+// app Data
+const appData = {
 	// user values
 	myAmount: 0,
 	myTotal: 0,
 	myGstAmount: 0,
 	myServiceChargeAmount: 0,
 	// gst and service charge rates
-	gstRate: undefined,
+	gstRate: 0,
 	serviceCharge: 10,
 };
 
-// get gst from data.gov.sg
-const ajaxGetGST = () => {};
+// ajax
+const ajaxGetGST = () => {
+	// resource ID for GST Tax rates
+	const resource_id = '86841ed8-9b1e-4af4-9ecf-bc860519702c';
 
-// calculate gst and service charge or both
+	$.ajax({
+		// get gst from data.gov.sg
+		url: `https://data.gov.sg/api/action/datastore_search?resource_id=${resource_id}`,
+	}).then((data) => {
+		//get current gst data
+		const currentGST = data.result.records[data.result.records.length - 1];
+		console.log(currentGST.tax_rate);
+	}),
+		() => {
+			console.log('bad request');
+		};
+};
+
+// calculate GST , service charge
 const calculateMyServiceCharge = () => {};
 
 const calculateMyGST = () => {};
@@ -22,4 +37,7 @@ const calculateMyGST = () => {};
 const removeLastValue = () => {};
 
 // start prog
-$(() => {});
+$(() => {
+	//test ajax
+	ajaxGetGST();
+});
