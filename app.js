186566a6275e8ec410f8df0b33f94941b1f1e5e8
Wenjie <wenjietj@gmail.com>
2021-03-09 00:52:20 +0800

    refactored ajax

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-// define percentage
+//////////////
+// CONSTANTS
+//////////////
+
+// percentage expression
 const PERCENT = 0.01;
 
-// app Data
-const appData = {
-	//display values
+// resource ID for GST Tax rates
+const GST_RESOURCE_ID = '86841ed8-9b1e-4af4-9ecf-bc860519702c';
+
+//////////////
+// APP OBJECT
+//////////////
+
+const app = {
+	// display values
 	displayAmount: '',
 	// user values
 	amount: 0,
@@ -11,77 +21,66 @@ const appData = {
 	gstAmount: 0,
 	serviceChargeAmount: 0,
 	// gst and service charge rates
-	gstRate: 0,
+	gstRate: undefined,
 	serviceCharge: 10,
 
 	// methods
-	calculateMyServiceCharge() {
+	calculateServiceCharge() {
 		this.serviceChargeAmount = this.amount * (this.serviceCharge * PERCENT);
 	},
-
-	calculateMyGST() {
+	calculateGST() {
 		this.gstAmount =
 			(this.amount + this.serviceChargeAmount) * (this.gstRate * PERCENT);
 	},
-
 	calculateTotal() {
 		this.total = this.amount + this.serviceChargeAmount + this.gstAmount;
 	},
+	removeLastValue() {},
 
 	// setters
 	setAmount(value) {
 		this.amount = Number(value);
 	},
-
 	setGSTRate(value) {
 		this.gstRate = value;
 	},
 };
 
-// ajax
-const ajaxGetGST = () => {
-	// resource ID for GST Tax rates
-	const resource_id = '86841ed8-9b1e-4af4-9ecf-bc860519702c';
+//////////////
+// APP START
+//////////////
 
+$(() => {
 	$.ajax({
 		// get gst from data.gov.sg
-		url: `https://data.gov.sg/api/action/datastore_search?resource_id=${resource_id}`,
+		url: `https://data.gov.sg/api/action/datastore_search?resource_id=${GST_RESOURCE_ID}`,
 	}).then((data) => {
-		//get current gst data
-		const currentGST = data.result.records[data.result.records.length - 1];
-		// console.log(currentGST.tax_rate);
-		appData.setGSTRate(currentGST);
+		// get current gst data
+		const currentGSTRate =
+			data.result.records[data.result.records.length - 1].tax_rate;
+		// set app with latest gst rate
+		app.setGSTRate(currentGSTRate);
+
+		// keypad events
+		$('.keypad').on('click', (event) => {
+			$('#before').empty();
+			app.displayAmount += $(event.currentTarget).text();
+			$('#before').text(app.displayAmount);
+			app.setAmount(app.displayAmount);
+		});
+
+		// delete event
+		$('#delete').on('click', (event) => {});
+
+		//  calculate event
+		$('#copy').on('click', () => {});
+
+		// copy event
+		$('#calculate').on('click', () => {});
 	}),
 		() => {
 			console.log('bad request');
 		};
-};
-
-// remove last value (backspace)
-const removeLastValue = () => {};
-
-// start prog
-$(() => {
-	//keypad events
-	$('.keypad').on('click', (event) => {
-		$('#before').empty();
-		appData.displayAmount += $(event.currentTarget).text();
-		$('#before').text(appData.displayAmount);
-		appData.setAmount(appData.displayAmount);
-	});
-
-	// delete event
-	$('#delete').on('click', (event) => {});
-
-	//calculate event
-	$('#copy').on('click', () => {});
-
-	//copy event
-	$('#calculate').on('click', () => {});
-
-	//test ajax
-	ajaxGetGST();
-	console.log(appData.gstRate);
 });
 
 // to limit one decimal per entry
